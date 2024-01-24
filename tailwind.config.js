fb0a8a681723d85e3afd446dc591f8a62c9670fa
tsxepo <tsxepo@gmail.com>
2024-01-24 15:08:50 +0200

    Updated colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}",],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedHover: "hsl(12, 88%, 70%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkBlueHover: "hsl(228, 39%, 60%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
